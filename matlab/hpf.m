dcf10165d48485ad091cf6ff2262732319fc861e
Bijan Fakhri <bfakhri@asu.edu>
2016-12-07 00:14:56 -0700

    Added new filters

---
+++ matlab/hpf.m
@@ -0,0 +1,9 @@
+% Creates FIR Filter
+N   = 600;        % FIR filter order
+Fp  = 1800;       % 1.8 kHz passband-edge frequency
+Fs  = 96e3;       % 96 kHz sampling frequency
+Rp  = 0.00057565; % Corresponds to 0.01 dB peak-to-peak ripple
+Rst = 1e-5;       % Corresponds to 80 dB stopband attenuation
+
+eqnum = firceqrip(N,Fp/(Fs/2),[Rp Rst],'high'); % eqnum = vec of coeffs
+fvtool(eqnum,'Fs',Fs,'Color','White') % Visualize filter
